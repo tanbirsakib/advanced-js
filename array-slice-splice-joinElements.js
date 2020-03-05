0e97557e2d195a2105cc453816aef16b9e5340b0
tanbirsakib <you@example.com>
2020-03-05 19:59:41 +0600

    finished exploring advanced javascript

---
+++ array-slice-splice-joinElements.js
@@ -0,0 +1,11 @@
+const nums =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//const part = nums.slice(3, 7); it bring out a section of an array
+//console.log(nums);
+//console.log(part);
+
+//const removed = nums.splice(2,6); it cut down/delete a section from an array
+//console.log(removed);
+//console.log(nums);
+
+const joinElements = nums.join("+"); //what we put between the "" it will show in console.log()
+console.log(joinElements);
