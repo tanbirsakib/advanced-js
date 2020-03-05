a56345dcea774c5f7ca2172fb78d7e3e4d2b9636
tanbirsakib <you@example.com>
2020-03-05 13:41:53 +0600

    added double vs tripple

---
+++ doubleVStrippleEqual.js
@@ -0,0 +1,46 @@
+/*const first = 2;
+
+const second = "2";
+if (first==second) {
+    console.log("The condition is true");
+}
+else{
+    console.log("The condition is false");
+}
+//this will be true.== check only the values
+*/
+
+/**const first = 2;
+
+const second = "2";
+
+if (first===second) {
+    console.log("The condition is true");
+}
+else{
+    console.log("The condition is false");
+}
+this will be false.=== check the value and type ass well.here second is a String.and first is a Number value
+*/
+/*const first = 1;
+
+const second = true;
+
+if (first==second) {
+    console.log("The condition is true");
+}
+else{
+    console.log("The condition is false");
+}
+this will be true.because 1 is assigned true in javascript and 0 assigned with false in javascript
+*/
+const first = 0;
+
+const second = false;
+
+if (first===second) {
+    console.log("The condition is true");
+}
+else{
+    console.log("The condition is false");
+}
